fd05bfd26e87ef902f95d20448b8c8c44f4c224a
retasu99 <182311959+retasu99@users.noreply.github.com>
2024-10-26 04:34:11 +0000

    delete_system installed

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/books/:id/edit' => 'books#edit', as: 'edit_book'
   patch '/books/:id' => 'books#update', as: 'update_book'
 
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
