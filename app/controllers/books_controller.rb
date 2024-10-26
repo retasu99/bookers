2c5b22e16fabb6e99eb8f1e6ee005caa9014002c
retasu99 <182311959+retasu99@users.noreply.github.com>
2024-10-26 05:21:24 +0000

    before installing varidation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 
-  def new
+  def index
     @books = Book.all
     @book = Book.new
   end
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books/new'
+    redirect_to '/books'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  get '/books/new' => 'books#new'
-  post '/books' => 'books#create'
-
-  # get '/books/edit' => 'books#edit'
-  get '/books/:id' => 'books#show', as: 'book'
-
-  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch '/books/:id' => 'books#update', as: 'update_book'
-
-  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+  # get '/books/new' => 'books#new'
+  # post '/books' => 'books#create'
+  # get '/books/:id' => 'books#show', as: 'book'
+  # get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch '/books/:id' => 'books#update', as: 'update_book'
+  # delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
 
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
