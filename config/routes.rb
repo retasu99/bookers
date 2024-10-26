9639ae9c77c94ac41f4d9660f4d7994795755030
retasu99 <182311959+retasu99@users.noreply.github.com>
2024-10-26 04:16:02 +0000

    edit_system installed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   get '/books/new' => 'books#new'
   post '/books' => 'books#create'
 
+  # get '/books/edit' => 'books#edit'
   get '/books/:id' => 'books#show', as: 'book'
 
-  get '/books/edit'
+  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch '/books/:id' => 'books#update', as: 'update_book'
+
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
